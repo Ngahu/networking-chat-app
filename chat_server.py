ec85eb26c73dabea6782ab39336604d718688de0
Ngahu <jamaalaraheem@gmail.com>
2016-09-04 15:20:11 +0300

    adding of the setting of the sockets and the port

---
+++ chat_server.py
@@ -0,0 +1,18 @@
+import  socket
+import time
+host = '127.0.0.1'
+port = 5000
+
+clients = []
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host,port))
+s.setblocking(0)
+
+quitting = False
+print "server started"
+
+while not quitting:
+    try:
+        data, addr = s.recvfrom(1024)
+        if "Quit" in str(data):
+            quitting = True
